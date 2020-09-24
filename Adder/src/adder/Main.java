5750cc9566cd21bb6437a863b02c96078ef92fac
Habib Kalia <habibkalia@gmail.com>
2020-09-24 16:33:25 -0500

    Change Adder to Support Multiple Integers
    Adder now supports adding multiple integers passed as arguments.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,16 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide at least two integers to add");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+        if (args.length < 2) throw new IllegalArgumentException();
+        int sum = 0;
+        for (String arg : args) {
+            sum += Integer.parseInt(arg);
+        }
+        return sum;
     }
 }
